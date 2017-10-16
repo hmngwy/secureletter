03f4e783e0f09f3920e4c632b2ae87f9d685d8d5
hmngwy <hmngwy@users.noreply.github.com>
2017-10-17 01:19:42 +0800

    Added publish function

--- methods.py
+++ methods.py
@@ -1,42 +1,10 @@
 import email
 import json
-
-
-def _get_ses_from_sns(event):
-    return json.loads(event['Records'][0]['Sns']['Message'])
-
-
-def _is_blocked(receipt):
-    verdicts = {k: v['status'] for k, v in receipt.items(
-    ) if k in ['spfVerdict', 'virusVerdict', 'dkimVerdict', 'spamVerdict']}
-    if not all(verd == 'PASS' for verd in list(verdicts.values())):
-        return True
-    return False
-
-
-def _get_signature(msg):
-    for part in msg.walk():
-        ctype = part.get_content_type()
-        cdispo = str(part.get('Content-Disposition'))
-
-        # If we find a signature, break
-        if ctype == 'application/pgp-signature' and 'attachment' in cdispo:
-            # Don't include headers in signature
-            signature = part.get_payload()
-            return signature
-
-    return None
-
-
-def _get_body(msg):
-    for part in msg.walk():
-        ctype = part.get_content_type()
-        cdispo = str(part.get('Content-Disposition'))
-
-        if ctype in ['text/plain', 'text/html'] and 'attachment' not in cdispo:
-            return part  # Whole part is signed, incl headers
-
-    return None
+import os
+from helpers import _is_blocked
+from helpers import _get_ses_from_sns
+from helpers import _get_signature
+from helpers import _get_body
 
 
 def unsubscribe(event, context):
@@ -53,38 +21,12 @@ def unsubscribe(event, context):
     from email.utils import parseaddr
 
     dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('subscribers')
+    table = dynamodb.Table(os.environ['DDB_SUBSCRIBERS_TABLE'])
     table.delete_item(
         Key={'pair': parseaddr(msg['From'])[
             1] + '-' + msg['Subject'].replace(' ', '')}
     )
     return 'UNSUBSCRIBE_SUCCESFUL'
-
-
-def test(event, context):
-
-    ses_message = _get_ses_from_sns(event)
-
-    if _is_blocked(ses_message['receipt']):
-        return "BLOCKED"
-
-    import boto3
-    from boto3.dynamodb.conditions import Attr, Key
-    from email.utils import parseaddr
-
-    msg = email.message_from_string(ses_message['content'])
-
-    dynamodb = boto3.resource('dynamodb')
-
-    # Newsletter exists
-    table = dynamodb.Table('subscribers')
-    response = table.query(
-        IndexName='newsletter_key-index',
-        KeyConditionExpression=Key('newsletter_key').eq(
-            msg['Subject'].replace(' ', ''))
-    )
-
-    return response
 
 
 def subscribe(event, context):
@@ -102,13 +44,13 @@ def subscribe(event, context):
     msg = email.message_from_string(ses_message['content'])
 
     dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('newsletters')
+    table = dynamodb.Table(os.environ['DDB_NEWSLETTERS_TABLE'])
     response = table.get_item(
         Key={'fingerprint': msg['Subject'].replace(' ', '')}
     )
     if 'Item' in response:
         # Newsletter exists
-        table = dynamodb.Table('subscribers')
+        table = dynamodb.Table(os.environ['DDB_SUBSCRIBERS_TABLE'])
         response = table.get_item(
             Key={'pair': parseaddr(msg['From'])[
                 1] + '-' + msg['Subject'].replace(' ', '')}
@@ -146,20 +88,18 @@ def register(event, context):
         return 'NONMULTIPART_EMAIL'
 
     # Look for signature
-    signature = _get_signature(b)
+    signature, signature_payload = _get_signature(b)
 
     # Look for email body
-    body = _get_body(b)
+    body, body_payload = _get_body(b)
 
     if not signature:
         return 'NO_SIGNATURE_PART'
 
-    if not body:
+    if not all(body):
         return 'NO_BODY_PART'
 
     import gnupg
-    import os
-
     from tempfile import TemporaryDirectory
     from tempfile import NamedTemporaryFile
 
@@ -176,9 +116,10 @@ def register(event, context):
             return 'KEY_NOT_FOUND ' + key
 
         with NamedTemporaryFile(delete=False, mode='w') as tmp:
-            tmp.write(str(signature))
+            tmp.write(signature_payload.decode('utf-8'))
             filename = tmp.name
 
+        # The whole ascii body "part" is signed
         verified = gpg.verify_data(filename, bytes(body))
         os.remove(filename)
 
@@ -196,7 +137,7 @@ def register(event, context):
         from email.utils import parseaddr
 
         dynamodb = boto3.resource('dynamodb')
-        table = dynamodb.Table('newsletters')
+        table = dynamodb.Table(os.environ['DDB_NEWSLETTERS_TABLE'])
 
         response = table.query(
             IndexName='email-index',
@@ -216,4 +157,93 @@ def register(event, context):
             }
         )
 
-        return 'SIGNATUREVALID_REGISTERED ',
+        return 'SIGNATUREVALID_REGISTERED '
+
+
+def publish(event, context):
+
+    ses_message = event['Records'][0]['ses']
+
+    if _is_blocked(ses_message['receipt']):
+        return "BLOCKED"
+
+    import boto3
+    import botocore
+    bucket = 'secureletter-posts-' + os.environ.get('TARGET', 'develop')
+    s3 = boto3.client('s3')
+    response = s3.get_object(
+        Bucket=bucket, Key=ses_message['mail']['messageId'])
+
+    # Read the raw text file into a Email Object
+    b = email.message_from_bytes(response['Body'].read())
+
+    if b.get_content_type() != 'multipart/signed':
+        return 'UNSIGNED_EMAIL'
+
+    if not b.is_multipart():
+        return 'NONMULTIPART_EMAIL'
+
+    # Look for signature
+    signature, signature_payload = _get_signature(b)
+
+    # Look for email body
+    body, body_payload = _get_body(b)
+
+    if not signature:
+        return 'NO_SIGNATURE_PART'
+
+    if not body:
+        return 'NO_BODY_PART'
+
+    # HERE THE EMAIL IS VALID
+
+    import boto3
+    from boto3.dynamodb.conditions import Key
+    import time
+    from email.utils import parseaddr
+
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(os.environ['DDB_NEWSLETTERS_TABLE'])
+
+    response = table.query(
+        IndexName='email-index',
+        KeyConditionExpression=Key('email').eq(
+            parseaddr(ses_message['mail']['source'])[1])
+    )
+
+    if not response['Count']:
+        return 'NOT_REGISTERED'
+    else:
+        key = response['Items'][0]['fingerprint']
+
+    import gnupg
+    from tempfile import TemporaryDirectory
+    from tempfile import NamedTemporaryFile
+
+    # Create an isolated GPG home, avoid cross-contamination
+    with TemporaryDirectory() as tmpdir:
+        print('created temporary directory', tmpdir)
+
+        gpg = gnupg.GPG(gnupghome=tmpdir)
+        import_res = gpg.recv_keys('keyserver.ubuntu.com', key)
+
+        if not import_res.fingerprints:
+            return 'KEY_NOT_FOUND ' + key
+
+        with NamedTemporaryFile(delete=False, mode='w') as tmp:
+            tmp.write(signature_payload.decode('utf-8'))
+            filename = tmp.name
+
+        # The whole ascii body "part" is signed
+        verified = gpg.verify_data(filename, bytes(body))
+        os.remove(filename)
+
+        if not verified:
+            return "DONOTTRUST"
+
+        if verified.key_status:
+            return "REVOKED_OR_EXPIRED_KEY"
+
+        # At this point we know that body is signed by user
+
+        return 'QUEUE_EMAILS'
