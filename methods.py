7c8a6cfa9fc0dba6da54eead4940044546b8b567
hmngwy <hmngwy@users.noreply.github.com>
2017-10-18 01:30:03 +0800

    Uses decorator for sig auth

--- methods.py
+++ methods.py
@@ -1,6 +1,7 @@
 import email
 import json
 import os
+from authenticate import authenticate
 from helpers import _is_blocked
 from helpers import _get_ses_from_sns
 from helpers import _get_signature
@@ -72,130 +73,11 @@ def subscribe(event, context):
     return msg['Subject'].replace(' ', '')
 
 
-def register(event, context):
-
-    ses_message = _get_ses_from_sns(event)
-
-    if _is_blocked(ses_message['receipt']):
-        return "BLOCKED"
-
-    b = email.message_from_string(ses_message['content'])
-
-    if b.get_content_type() != 'multipart/signed':
-        return 'UNSIGNED_EMAIL'
-
-    if not b.is_multipart():
-        return 'NONMULTIPART_EMAIL'
-
-    # Look for signature
-    signature, signature_payload = _get_signature(b)
-
-    # Look for email body
-    body, body_payload = _get_body(b)
-
-    if not signature:
-        return 'NO_SIGNATURE_PART'
-
-    if not all(body):
-        return 'NO_BODY_PART'
-
-    import gnupg
-    from tempfile import TemporaryDirectory
-    from tempfile import NamedTemporaryFile
-
-    key = b['Subject'].strip()
-
-    # Create an isolated GPG home, avoid cross-contamination
-    with TemporaryDirectory() as tmpdir:
-        print('created temporary directory', tmpdir)
-
-        gpg = gnupg.GPG(gnupghome=tmpdir)
-        import_res = gpg.recv_keys('keyserver.ubuntu.com', key)
-
-        if not import_res.fingerprints:
-            return 'KEY_NOT_FOUND ' + key
-
-        with NamedTemporaryFile(delete=False, mode='w') as tmp:
-            tmp.write(signature_payload.decode('utf-8'))
-            filename = tmp.name
-
-        # The whole ascii body "part" is signed
-        verified = gpg.verify_data(filename, bytes(body))
-        os.remove(filename)
-
-        if not verified:
-            return "DONOTTRUST"
-
-        if verified.key_status:
-            return "REVOKED_OR_EXPIRED_KEY"
-
-        # At this point we know that body is signed by user
-
-        import boto3
-        from boto3.dynamodb.conditions import Key
-        import time
-        from email.utils import parseaddr
-
-        dynamodb = boto3.resource('dynamodb')
-        table = dynamodb.Table(os.environ['DDB_NEWSLETTERS_TABLE'])
-
-        response = table.query(
-            IndexName='email-index',
-            KeyConditionExpression=Key('email').eq(
-                parseaddr(verified.username)[1])
-        )
-
-        if response['Count']:
-            return 'ALREADY_REGISTERED'
-
-        table.put_item(
-            Item={
-                'fingerprint': verified.fingerprint,
-                'username': verified.username,
-                'created_on': int(time.time()),
-                'email': parseaddr(verified.username)[1]
-            }
-        )
-
-        return 'SIGNATUREVALID_REGISTERED '
-
-
-def publish(event, context):
-
-    ses_message = event['Records'][0]['ses']
-
-    if _is_blocked(ses_message['receipt']):
-        return "BLOCKED"
-
-    import boto3
-    import botocore
-    bucket = 'secureletter-posts-' + os.environ.get('TARGET', 'develop')
-    s3 = boto3.client('s3')
-    response = s3.get_object(
-        Bucket=bucket, Key=ses_message['mail']['messageId'])
-
-    # Read the raw text file into a Email Object
-    b = email.message_from_bytes(response['Body'].read())
-
-    if b.get_content_type() != 'multipart/signed':
-        return 'UNSIGNED_EMAIL'
-
-    if not b.is_multipart():
-        return 'NONMULTIPART_EMAIL'
-
-    # Look for signature
-    signature, signature_payload = _get_signature(b)
-
-    # Look for email body
-    body, body_payload = _get_body(b)
-
-    if not signature:
-        return 'NO_SIGNATURE_PART'
-
-    if not body:
-        return 'NO_BODY_PART'
-
-    # HERE THE EMAIL IS VALID
+@authenticate(fingerprint_from='subject', ses_from='SNS')
+def register(event, context, *args, **kwargs):
+    verified = kwargs.get('verified')
+    ses_message = kwargs.get('ses_message')
+    mail = kwargs.get('mail')
 
     import boto3
     from boto3.dynamodb.conditions import Key
@@ -208,42 +90,28 @@ def publish(event, context):
     response = table.query(
         IndexName='email-index',
         KeyConditionExpression=Key('email').eq(
-            parseaddr(ses_message['mail']['source'])[1])
+            parseaddr(verified.username)[1])
     )
 
-    if not response['Count']:
-        return 'NOT_REGISTERED'
-    else:
-        key = response['Items'][0]['fingerprint']
+    if response['Count']:
+        return 'ALREADY_REGISTERED'
 
-    import gnupg
-    from tempfile import TemporaryDirectory
-    from tempfile import NamedTemporaryFile
+    table.put_item(
+        Item={
+            'fingerprint': verified.fingerprint,
+            'username': verified.username,
+            'created_on': int(time.time()),
+            'email': parseaddr(verified.username)[1]
+        }
+    )
 
-    # Create an isolated GPG home, avoid cross-contamination
-    with TemporaryDirectory() as tmpdir:
-        print('created temporary directory', tmpdir)
+    return 'SIGNATUREVALID_REGISTERED '
 
-        gpg = gnupg.GPG(gnupghome=tmpdir)
-        import_res = gpg.recv_keys('keyserver.ubuntu.com', key)
 
-        if not import_res.fingerprints:
-            return 'KEY_NOT_FOUND ' + key
+@authenticate(content_from='s3', ses_from='SES')
+def publish(event, context, *args, **kwargs):
+    verified = kwargs.get('verified')
+    ses_message = kwargs.get('ses_message')
+    mail = kwargs.get('mail')
 
-        with NamedTemporaryFile(delete=False, mode='w') as tmp:
-            tmp.write(signature_payload.decode('utf-8'))
-            filename = tmp.name
-
-        # The whole ascii body "part" is signed
-        verified = gpg.verify_data(filename, bytes(body))
-        os.remove(filename)
-
-        if not verified:
-            return "DONOTTRUST"
-
-        if verified.key_status:
-            return "REVOKED_OR_EXPIRED_KEY"
-
-        # At this point we know that body is signed by user
-
-        return 'QUEUE_EMAILS'
+    return 'QUEUE_EMAILS'
