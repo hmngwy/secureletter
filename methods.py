b188ca037622f31d3014f1c11fda1a9225cca705
hmngwy <hmngwy@users.noreply.github.com>
2017-10-19 01:57:53 +0800

    Moved checks into decorators

--- methods.py
+++ methods.py
@@ -1,28 +1,25 @@
 import email
 import json
 import os
-from authenticate import authenticate
-from helpers import _is_blocked
-from helpers import _get_ses_from_sns
-from helpers import _get_signature
-from helpers import _get_body
+from email.utils import parseaddr
+
+import boto3
+from boto3.dynamodb.conditions import Attr, Key
+
+from decorators import authenticate
+from decorators import get_ses_message
+from decorators import is_not_blocked
 
 
-def unsubscribe(event, context):
-
-    ses_message = _get_ses_from_sns(event)
-
-    if _is_blocked(ses_message['receipt']):
-        return "BLOCKED"
+@get_ses_message(ses_from='SNS')
+@is_not_blocked
+def unsubscribe(event, context, ses_message):
 
     msg = email.message_from_string(ses_message['content'])
 
-    import boto3
-    from boto3.dynamodb.conditions import Attr, Key
-    from email.utils import parseaddr
-
     dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table(os.environ['DDB_SUBSCRIBERS_TABLE'])
+    table = dynamodb.Table(os.environ.get(
+        'DDB_SUBSCRIBERS_TABLE', 'subscribers-develop'))
     table.delete_item(
         Key={'pair': parseaddr(msg['From'])[
             1] + '-' + msg['Subject'].replace(' ', '')}
@@ -30,28 +27,22 @@ def unsubscribe(event, context):
     return 'UNSUBSCRIBE_SUCCESFUL'
 
 
-def subscribe(event, context):
-
-    ses_message = _get_ses_from_sns(event)
-
-    if _is_blocked(ses_message['receipt']):
-        return "BLOCKED"
-
-    import boto3
-    from email.utils import parseaddr
-    from boto3.dynamodb.conditions import Key
-    from boto3.dynamodb.conditions import Attr
+@get_ses_message(ses_from='SNS')
+@is_not_blocked
+def subscribe(event, context, ses_message):
 
     msg = email.message_from_string(ses_message['content'])
 
     dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table(os.environ['DDB_NEWSLETTERS_TABLE'])
+    table = dynamodb.Table(os.environ.get(
+        'DDB_NEWSLETTERS_TABLE', 'newsletters-develop'))
     response = table.get_item(
         Key={'fingerprint': msg['Subject'].replace(' ', '')}
     )
     if 'Item' in response:
         # Newsletter exists
-        table = dynamodb.Table(os.environ['DDB_SUBSCRIBERS_TABLE'])
+        table = dynamodb.Table(os.environ.get(
+            'DDB_SUBSCRIBERS_TABLE', 'subscribers-develop'))
         response = table.get_item(
             Key={'pair': parseaddr(msg['From'])[
                 1] + '-' + msg['Subject'].replace(' ', '')}
@@ -73,19 +64,18 @@ def subscribe(event, context):
     return msg['Subject'].replace(' ', '')
 
 
-@authenticate(fingerprint_from='subject', ses_from='SNS')
+@get_ses_message(ses_from='SNS')
+@is_not_blocked
+@authenticate(fingerprint_from='subject')
 def register(event, context, *args, **kwargs):
+
     verified = kwargs.get('verified')
     ses_message = kwargs.get('ses_message')
     mail = kwargs.get('mail')
 
-    import boto3
-    from boto3.dynamodb.conditions import Key
-    import time
-    from email.utils import parseaddr
-
     dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table(os.environ['DDB_NEWSLETTERS_TABLE'])
+    table = dynamodb.Table(os.environ.get(
+        'DDB_NEWSLETTERS_TABLE', 'newsletters-develop'))
 
     response = table.query(
         IndexName='email-index',
@@ -108,7 +98,9 @@ def register(event, context, *args, **kwargs):
     return 'SIGNATUREVALID_REGISTERED '
 
 
-@authenticate(content_from='s3', ses_from='SES')
+@get_ses_message(ses_from='SES')
+@is_not_blocked
+@authenticate(content_from='s3')
 def publish(event, context, *args, **kwargs):
     verified = kwargs.get('verified')
     ses_message = kwargs.get('ses_message')
